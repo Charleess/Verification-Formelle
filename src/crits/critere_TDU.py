7eeb05796933645c978278ffb14f79c91071e4dd
Karim Lasri <karim.lasri@student.ecp.fr>
2018-02-10 13:08:54 +0100

    Edited crits TU/TDU

--- src/crits/critere_TDU.py
+++ src/crits/critere_TDU.py
@@ -7,22 +7,11 @@ def elems_to_cover_TDU(graph):
         node for node in graph.nodes if Def(graph, node) != []
     ] # All the definition nodes
 
-    return def_nodes_list
-
-def test_all_usages_paths(graph, tests, elems_to_cover):
-    """ Test the criteria """
-    tests_paths = []
-    for t in tests:
-        # Run the test
-        path = browse_graph(t, graph) # Get the path associated with the test
-        tests_paths.append(path)
-
-    def_node_list = elems_to_cover
     ref_node_list = [node for node in graph.nodes if Ref(graph, node) != []] # All the reference nodes
-    successes = []
+
     possible_paths_dico = {} # Holds the state of the test run
 
-    for node in def_node_list: # For every definition ...
+    for node in def_nodes_list: # For every definition ...
         possible_paths_dico[node] = {}
         def_var = Def(graph, node)[0] # The defined variable, there can be only one
         ref_nodes = [n for n in ref_node_list if def_var in Ref(graph, n)] # All the nodes that refer to this variable
@@ -39,28 +28,56 @@ def test_all_usages_paths(graph, tests, elems_to_cover):
                 if is_valid:
                     possible_paths_dico[node][ref_node].append(path) # This path is ok
             # Now we have all the paths that go from u to v and do not redefine the variable defined in u
+            if possible_paths_dico[node][ref_node] == [] or possible_paths_dico[node][ref_node] == [[node]]:
+                possible_paths_dico[node].pop(ref_node)
+    return possible_paths_dico
 
-    for u in def_node_list: # We can now check the criterion
-        all_ok = True
+def get_elems_to_cover_paths(possible_paths_dico):
+    elems_paths = []
+    for u in possible_paths_dico.keys():
         for v in possible_paths_dico[u].keys():
-            all_ok_v = True
-            for subpath in possible_paths_dico[u][v]:
-                all_ok_subpath = False
-                for test_path in tests_paths:
-                    if len(subfinder(test_path, subpath)) > 0: # The path was found in at least one of the tests
-                        all_ok_subpath = True
-                all_ok_v = all_ok_v and all_ok_subpath # Here is the difference with the TU criterion
-            all_ok = all_ok and all_ok_v # FOR ALL
-        if all_ok:
-            successes.append(u)
+            for path in possible_paths_dico[u][v]:
+                elems_paths.append(path)
+    return elems_paths
 
-    return(def_node_list, successes)
+def test_all_usages_paths(graph, tests, elems_to_cover):
+    """ Test the criteria """
+    tests_paths = []
+    for t in tests:
+        # Run the test
+        path = browse_graph(t, graph) # Get the path associated with the test
+        tests_paths.append(path)
+
+    successes = []
+    possible_paths_dico = elems_to_cover
+    paths_to_cover = get_elems_to_cover_paths(possible_paths_dico)
+    paths_to_cover_fix = paths_to_cover
+
+    # for u in possible_paths_dico.keys(): # We can now check the criterion
+    #     all_ok = True
+    #     for v in possible_paths_dico[u].keys():
+    #         all_ok_v = True
+    #         for subpath in possible_paths_dico[u][v]:
+    #             all_ok_subpath = False
+    #             for test_path in tests_paths:
+    #                 if len(subfinder(test_path, subpath)) > 0: # The path was found in at least one of the tests
+    #                     all_ok_subpath = True
+    #             all_ok_v = all_ok_v and all_ok_subpath # Here is the difference with the TU criterion
+    #         all_ok = all_ok and all_ok_v # FOR ALL
+    #     if all_ok:
+    #         successes.append(u)
+    for path in paths_to_cover_fix:
+        for test_path in tests_paths:
+            if len(subfinder(test_path, path)) > 0:  # The path was found in at least one of the tests
+                paths_to_cover = [p for p in paths_to_cover if p != path]
+
+    return(paths_to_cover_fix, paths_to_cover)
 
 def critere_TDU(graph, tests, elems_to_cover):
     """ Main """
-    def_node_list, successes = test_all_usages_paths(graph, tests, elems_to_cover)
+    paths_to_cover_fix, paths_to_cover = test_all_usages_paths(graph, tests, elems_to_cover)
     try:
-        res = (len(successes) / len(def_node_list)) * 100
-        return res, list(set(def_node_list) - set(successes))
+        res = (1 - len(paths_to_cover) / len(paths_to_cover_fix)) * 100
+        return res, paths_to_cover
     except ValueError:
         return None
--- src/crits/critere_TU.py
+++ src/crits/critere_TU.py
@@ -7,22 +7,11 @@ def elems_to_cover_TU(graph):
         node for node in graph.nodes if Def(graph, node) != []
     ] # All the definition nodes
 
-    return def_nodes_list
-
-def test_all_usages(graph, tests, elems_to_cover):
-    """ Test the criteria """
-    tests_paths = []
-    for t in tests:
-        # Run the test
-        path = browse_graph(t, graph) # Get the path associated with the test
-        tests_paths.append(path)
-
-    def_node_list = elems_to_cover
     ref_node_list = [node for node in graph.nodes if Ref(graph, node) != []] # All the reference nodes
-    successes = []
+
     possible_paths_dico = {} # Holds the state of the test run
 
-    for node in def_node_list: # For every definition ...
+    for node in def_nodes_list: # For every definition ...
         possible_paths_dico[node] = {}
         def_var = Def(graph, node)[0] # The defined variable, there can be only one
         ref_nodes = [n for n in ref_node_list if def_var in Ref(graph, n)] # All the nodes that refer to this variable
@@ -39,27 +28,51 @@ def test_all_usages(graph, tests, elems_to_cover):
                 if is_valid:
                     possible_paths_dico[node][ref_node].append(path) # This path is ok
             # Now we have all the paths that go from u to v and do not redefine the variable defined in u
+            if possible_paths_dico[node][ref_node] == [] or possible_paths_dico[node][ref_node] == [[node]]:
+                possible_paths_dico[node].pop(ref_node)
+    return possible_paths_dico
 
-    for u in def_node_list: # We can now check the criterion
-        all_ok = True
+
+def get_elems_to_cover_idx(possible_paths_dico):
+    elems_idx = []
+    for u in possible_paths_dico.keys():
         for v in possible_paths_dico[u].keys():
-            all_ok_v = False or possible_paths_dico[u][v] == []
+            elems_idx.append([u, v])
+    return elems_idx
+
+def test_all_usages(graph, tests, elems_to_cover):
+    """ Test the criteria """
+    tests_paths = []
+    for t in tests:
+        # Run the test
+        path = browse_graph(t, graph) # Get the path associated with the test
+        tests_paths.append(path)
+
+    possible_paths_dico = elems_to_cover
+    elems_to_cover_idx = get_elems_to_cover_idx(possible_paths_dico)
+    elems_to_cover_idx_fix = elems_to_cover_idx
+
+    for u in possible_paths_dico.keys(): # We can now check the criterion
+        #all_ok = True
+        for v in possible_paths_dico[u].keys():
+            all_ok_v = False
             for subpath in possible_paths_dico[u][v]:
                 for test_path in tests_paths:
                     if len(subfinder(test_path, subpath)) > 0: # The path was found in at least one of the tests
                         all_ok_v = True
-            all_ok = all_ok and all_ok_v # AT LEAST and not FOR ALL
-        if all_ok:
-            successes.append(u)
+            if all_ok_v:
+                elems_to_cover_idx = [[a, b] for [a, b] in elems_to_cover_idx if [a, b] !=[u, v]]
+            #all_ok = all_ok and all_ok_v # AT LEAST and not FOR ALL
+        #if all_ok:
+         #   successes.append(u)
 
-    return(def_node_list, successes)
+    return(elems_to_cover_idx_fix, elems_to_cover_idx)
 
 def critere_TU(graph, tests, elems_to_cover):
     """ Main """
-    def_node_list, successes = test_all_usages(graph, tests, elems_to_cover)
-
+    elems_to_cover, elems_not_covered = test_all_usages(graph, tests, elems_to_cover)
     try:
-        res = (len(successes) / len(def_node_list)) * 100
-        return res, list(set(def_node_list) - set(successes))
+        res = (1-len(elems_not_covered) / len(elems_to_cover)) * 100
+        return res, elems_not_covered
     except ValueError:
         return None
